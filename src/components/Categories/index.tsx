fbb7df70d42cf98934910fa8987e7d7bc245eac7
Jonatas Alves <jonatas.a2012@hotmail.com>
2023-03-30 00:41:14 -0300

    Finalized Categories FlatList

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Category, Icon } from './styles';
 
@@ -5,6 +6,13 @@ import { categories } from '../../mocks/categories';
 import { Text } from '../Text';
 
 export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
   return (
     <FlatList
       horizontal
@@ -12,14 +20,18 @@ export function Categories() {
       data={categories}
       contentContainerStyle={{ paddingRight: 24 }}
       keyExtractor={category => category._id}
-      renderItem={({ item: category }) => (
-        <Category>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
-          <Text size={14} weight="600">{category.name}</Text>
-        </Category>
-      )}
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
+
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+            </Icon>
+            <Text size={14} weight="600">{category.name}</Text>
+          </Category>
+        );
+      }}
     />
   );
 }
